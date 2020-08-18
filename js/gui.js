ac90bd85754e069104ac6c5fa9d43b016c8450b9
Luis Victoria <luisvictoriaperez@gmail.com>
2020-08-18 02:14:20 -0400

    Import piece images and place onto board GUI

--- js/gui.js
+++ js/gui.js
@@ -1,6 +1,36 @@
 $("#SetFen").click(function () {
 	var fenStr = $("#fenIn").val(); // .val() will get value of whatever is inside the textbox
 	ParseFen(fenStr);
-	PrintBoard();
-	SearchPosition();
+	NewGame(fenStr);
 });
+
+function NewGame(fenStr) {
+	ParseFen(fenStr);
+	PrintBoard();
+	SetInitialBoardPieces();
+}
+
+function ClearAllPieces() {
+	$(".Piece").remove();
+}
+
+function SetInitialBoardPieces() {
+	var sq, sq120, file, rank, rankName, fileName, imgString, pieceImgName, piece;
+
+	ClearAllPieces();
+
+	for(sq = 0; sq < 64; sq++) {
+		sq120 = SQ120(sq);
+		piece = GameBoard.pieces[sq120];
+		file = FilesBrd[sq120];
+		rank = RanksBrd[sq120];
+
+		if(piece >= PIECES.wP && piece <= PIECES.bK) {
+			rankName = "rank" + (rank + 1);	// must add 1 since it is 0-indexed
+			fileName = "file" + (file + 1);
+			pieceImgName = "img/" + SideChar[PieceCol[piece]] + PceChar[piece].toUpperCase() + ".png";
+			imgString = "<image src=\"" + pieceImgName + "\" class=\"Piece " + rankName + " " + fileName + "\"/>";
+			$("#Board").append(imgString);
+		}
+	}
+}
